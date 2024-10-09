05d8ec491e7bcfe930ae8f07afe4494c1e31d56d
matan-ode <matanodentz@gmail.com>
2024-10-09 13:15:58 +0300

    Init repo

---
+++ cmps/BooksPreview.jsx
@@ -0,0 +1,8 @@
+
+export function BooksPreview() {
+    
+    return (
+        <section className="books-preview">
+        </section>
+    )
+}
---
+++ pages/Home.jsx
@@ -0,0 +1,10 @@
+import { BooksPreview } from '../cmps/BooksPreview.jsx'
+
+export function Home() {
+    return (
+        <section>
+            <h2>Our books:</h2>
+            <BooksPreview />
+        </section>
+    )
+}
